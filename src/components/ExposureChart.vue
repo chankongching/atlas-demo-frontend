9c4f14dc96c67787774c491356496ae18bb2fa74
joycean <ljmail@vip.163.com>
2019-08-09 19:17:51 +0800

    add bi page

---
+++ src/components/ExposureChart.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="exposure-chart-item" :class="newData.length > 0 ? '' : 'add-exposure-chart-item'">
+    <span v-for="item of newData" :key="item.name" v-bind:style="{ backgroundColor: color16(), flex: [0, 0, 200 / item.value + '%'] }">
+     <i>{{ item.name }}</i>
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+import Ant from 'ant-design-vue';
+
+@Component({
+  components: {
+    
+  },
+})
+export default class ExposureChart extends Vue {
+  private newData: Array<object> = [];
+
+  @Prop({required: false, type: Array, default: () => [
+    
+  ]})
+  data ?: []
+  
+  color16(){ // 十六进制颜色随机
+    var r = Math.floor(Math.random()*256);
+    var g = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
+    var color = '#'+r.toString(16)+g.toString(16)+b.toString(16);
+    return color;
+  }
+
+  @Watch('data', {immediate: true, deep: true})
+  watchData(newVal: Array<object>, oldVal: Array<object>) {
+    this.newData = newVal;
+  }
+}
+</script>
+<style lang="scss" scoped>
+.exposure-chart-item {
+  width: 200px;
+  height: 40px;
+  border-radius: 8px;
+  display: flex;
+  overflow: hidden;
+  & * {
+    margin: 0;
+    padding: 0;
+  }
+  span {
+    height: 100%;
+  }
+  &.add-exposure-chart-item {
+    border: 1px dashed #A6A8AA;
+  }
+}
+</style>
+
--- src/views/Business-Intel.vue
+++ src/views/Business-Intel.vue
@@ -18,6 +18,20 @@
             <h6 class="selected-item-wallet">wallets</h6>
           </div>
         </div>
+        <div class="selected-item">
+          <div class="selected-item-top">
+            <h3 class="selected-item-name">Binance</h3>
+            <h5 class="selected-item-type">eXCHANGE</h5>
+            <p class="selected-item-source">User uploaded wallet</p>
+          </div>
+          <div class="selected-item-bottom">
+            <h2 class="selected-item-balance">1,434,394</h2>
+            <h6 class="selected-item-wallet">wallets</h6>
+          </div>
+        </div>
+        <div class="selected-item add-selected-item">
+          <a-icon type="plus" />
+        </div>
       </div>
     </div>
     <div style="padding: 0 20px;">
@@ -25,12 +39,27 @@
     </div>
     <div style="padding: 0 20px;">
       <h3 class="item-title">IN</h3>
+      <div class="exposure-chart-container">
+        <exposure-chart :data="exposureChartData"/>
+        <exposure-chart :data="exposureChartData"/>
+        <exposure-chart :data="exposureChartData"/>
+        <exposure-chart :data="[]"/>
+      </div>
     </div>
     <div style="padding: 0 20px;">
       <h3 class="item-title">OUT</h3>
+      <div class="exposure-chart-container">
+        <exposure-chart :data="exposureChartData"/>
+        <exposure-chart :data="exposureChartData"/>
+        <exposure-chart :data="exposureChartData"/>
+        <exposure-chart :data="[]"/>
+      </div>
     </div>
     <div style="padding: 0 20px;">
       <h3 class="item-title">Fund distrubution</h3>
+      <div>
+        <v-chart :options="fundDistrubution" autoresize/>
+      </div>
     </div>
     <div style="padding: 0 20px;">
       <h3 class="item-title">User growth</h3>
@@ -44,13 +73,43 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Ant from 'ant-design-vue';
+import ExposureChart from '@/components/ExposureChart.vue'
+import 'echarts/lib/chart/bar'
 
 @Component({
   components: {
+    ExposureChart,
     AIcon: Ant.Icon,
   },
 })
-export default class BusinessInterPage extends Vue {}
+export default class BusinessInterPage extends Vue {
+  private exposureChartData = [
+    {name: 'a1', value: 20},
+    {name: 'a2', value: 14},
+    {name: 'a3', value: 50},
+    {name: 'a4', value: 30},
+    {name: 'a5', value: 5}
+  ];
+  private fundDistrubution = {
+      tooltip: {},
+      dataset: {
+          source: [
+              ['product', '2015', '2016', '2017'],
+              ['Matcha Latte', 43.3, 85.8, 93.7],
+              ['Milk Tea', 83.1, 73.4, 55.1],
+              ['Cheese Cocoa', 86.4, 65.2, 82.5],
+              ['Walnut Brownie', 72.4, 53.9, 39.1]
+          ]
+      },
+      xAxis: {type: 'category'},
+      yAxis: {},
+      series: [
+          {type: 'bar'},
+          {type: 'bar'},
+          {type: 'bar'}
+      ]
+  };
+}
 </script>
 <style lang="scss" scoped>
 .flex-between {
@@ -71,6 +130,10 @@ export default class BusinessInterPage extends Vue {}
   font-weight: 200;
 }
 .selected-container {
+  display: flex;
+  & .selected-item:last-child  {
+    margin-right: 0;
+  }
   .selected-item {
     width: 200px;
     height: 200px;
@@ -81,15 +144,28 @@ export default class BusinessInterPage extends Vue {}
     background-color: #fff;
     box-shadow: 0 0 20px 2px #0000000a;
     padding: 10px;
-    .selected-item-top {
-      & * {
-        margin: 0;
-        padding: 0;
+    border-bottom: 10px solid #256BCE;
+    margin-right: 20px;
+    & * {
+      margin: 0;
+      padding: 0;
+    }
+    &.add-selected-item {
+      box-shadow: none;
+      border: 1px dashed #A6A8AA;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .anticon {
+        font-size: 30px;
+        font-weight: 200;
       }
+    }
+    .selected-item-top {
       .selected-item-name {
         color: #256BCE;
         font-weight: 600;
-        font-size: 32px;
+        font-size: 30px;
       }
       .selected-item-type {
         color: #A6A8AA;
@@ -97,25 +173,34 @@ export default class BusinessInterPage extends Vue {}
         font-weight: 400;
       }
       .selected-item-source {
-        font-size: 14px;
+        font-size: 16px;
         color: #1F262A;
         font-weight: 200;
       }
     }
     .selected-item-bottom {
       .selected-item-balance {
-        font-size: 42px;
+        font-size: 32px;
         color: #256BCE;
         font-weight: 600;
         text-align: right;
       }
       .selected-item-wallet {
         font-weight: 400;
-        font-size: 14px;
+        font-size: 16px;
         color: #1F262A;
         text-align: right;
       }
     }
+  }
+}
+.exposure-chart-container {
+  display: flex;
+  .exposure-chart-item {
+    margin-right: 20px;
+  }
+  & .exposure-chart-item:last-child  {
+    margin-right: 0;
   }
 }
 </style>
